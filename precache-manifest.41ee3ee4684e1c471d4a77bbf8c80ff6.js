ee4824af2976695788f5eeca1f4618db725396ce
Fatem <f.s4remi@gmail.com>
2020-04-28 00:33:11 -0700

    Updates

---
+++ precache-manifest.41ee3ee4684e1c471d4a77bbf8c80ff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562d6edd5725b4e0640afa296ed47dee",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "d85abc6391874adbd914",
+    "url": "/ReactApp/static/js/2.64dd9fc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactApp/static/js/2.64dd9fc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cddeef31994f6b888c9",
+    "url": "/ReactApp/static/js/main.64bf962c.chunk.js"
+  },
+  {
+    "revision": "7dc6b2e3a76f28babbb3",
+    "url": "/ReactApp/static/js/runtime-main.790250c2.js"
+  },
+  {
+    "revision": "17a4633919e490a28a253270176168a5",
+    "url": "/ReactApp/static/media/header.17a46339.png"
+  }
+]);
